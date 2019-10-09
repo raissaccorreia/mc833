f416452839580520386dd5e68998a743762a7489
Raissa Correia <raisssa.correia@gmail.com>
2019-10-09 13:05:06 -0300

    corrigindo bug do loop while(1)

--- MC833_new/lab4/cliente.c
+++ MC833_new/lab4/cliente.c
@@ -13,7 +13,7 @@
 #define MAXLINE 4096
 
 int main(int argc, char **argv) {
-   int    sockfd, n;
+   int    sockfd, n, i;
    char   recvline[MAXLINE + 1];
    char   entrada[MAXLINE + 1];
    char   error[MAXLINE + 1];
@@ -34,14 +34,14 @@ int main(int argc, char **argv) {
       exit(1);
    }
 
-   bzero(&servaddr, sizeof(servaddr));     
-  
+   bzero(&servaddr, sizeof(servaddr));
+
    servaddr.sin_family = AF_INET;
    servaddr.sin_port   = htons(8000);
    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
       perror("inet_pton error");
       exit(1);
-   }   
+   }
 
    /*cliente se conecta com o servidor remeto, 0 ok, -1 erro */
    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
@@ -50,9 +50,9 @@ int main(int argc, char **argv) {
    }
 
    /* enquanto o servidor enviar linha nao vazia realiza a leitra, se nao EOF */
-   while (1) {
+   for (i=0;i<=5;i++) {
       n = recv(sockfd, recvline, sizeof(recvline),0);
-      recvline[n] = '\0';            
+      recvline[n] = '\0';
       getsockname(sockfd, (struct sockaddr *) &servaddr, &len);
       printf("Peer IP adress: %s\n", inet_ntoa(servaddr.sin_addr));
       printf("Local port: %u\n", servaddr.sin_port);
@@ -74,13 +74,12 @@ int main(int argc, char **argv) {
       //    printf("Erro, nao foi possivel abrir o arquivo\n");
       // else{
       //    fprintf(log2, "%s\n", entrada);
-      // }  
-   } 
-   if (n < 0) {      
+      // }
+   }
+   if (n < 0) {
       perror("read error");
       exit(1);
    }
    // fclose(log2);
    exit(0);
 }
- 
