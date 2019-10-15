c13fba1e229fd3a42fdd815cbdb8763e3987a380
Raissa Cavalcante Correia <ra150619@kiss.lab.ic.unicamp.br>
2019-10-14 21:35:41 -0300

    bug da letra comida resolvido

--- MC833_new/lab4/cliente.c
+++ MC833_new/lab4/cliente.c
@@ -29,8 +29,7 @@ int main(int argc, char **argv) {
    char   message[MAXLINE + 1];
    char   error[MAXLINE + 1];
    struct sockaddr_in servaddr;
-   fd_set rset;
-   //socklen_t len = sizeof(servaddr);
+   fd_set rset ;
 
 /*confirmando numero de parametros passados, apenas IP do servidor*/
    if (argc != 3) {
@@ -52,20 +51,19 @@ int main(int argc, char **argv) {
    Connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
    /* enquanto o servidor enviar linha nao vazia realiza a leitra, se nao EOF */
-   for ( ; ; ) {
-
-     FD_ZERO(&rset);
+   FD_ZERO(&rset);  
+   for ( ; ; ) {        
      FD_SET(fileno(stdin), &rset);
      FD_SET(sockfd, &rset);
      int maxfdp1 = max(fileno(stdin), sockfd) + 1;
      select(maxfdp1, &rset, NULL, NULL, NULL);
 
      //pegando recvline do arquivo
-     if (FD_ISSET(sockfd, &rset)){
+     if (FD_ISSET(sockfd, &rset)){       
        if (fgets(recvline, MAXLINE, stdin) == NULL){
           exit(0);
         }
-     }
+     }     
     //escrevendo mensagem para o servidor
      if (FD_ISSET(fileno(stdin), &rset)){
        if(fgets(message, MAXLINE, stdin) != NULL){
@@ -74,14 +72,8 @@ int main(int argc, char **argv) {
          shutdown(sockfd, SHUT_WR);
          return 0;
        }
-
        fputs(message, stdout);
-     }
-     //chegou no final do arquivo encerrar
-      if(getchar()==EOF) {
-         close(sockfd);
-         exit(1);
-      }
+     }     
    }
    //para o caso de um erro de leitura
    if (n < 0) {
@@ -90,31 +82,3 @@ int main(int argc, char **argv) {
    }
    exit(0);
 }
-
-//residuos da versao antiga
-//pegando string enviada pelo servidor
-//n = recv(sockfd, recvline, sizeof(recvline),0);
-//recvline[n] = '\0';
-/*criando socket do lado do cliente*/
-//getsockname(sockfd, (struct sockaddr *) &servaddr, &len);
-//printf("Peer IP adress: %s\n", inet_ntoa(servaddr.sin_addr));
-//printf("Local port: %u\n", servaddr.sin_port);
-
-/*le da message padrao*/
-//scanf("%s", message);
-//confirmacao do que foi enviado
-//printf("Input do Clientee: %s\n", message);
-
-//enviando ao servidor
-//send(sockfd, message, sizeof(message), 0);
-//receba do servidor
-//recv(sockfd, message, sizeof(message), 0);
-//message agora é o que foi recebido do servidor
-//printf("%s\n", message);
-// log2 = fopen("log2.txt", "w");
-// if(log2 == NULL)
-//    printf("Erro, nao foi possivel abrir o arquivo\n");
-// else{
-//    fprintf(log2, "%s\n", message);
-// }
-//if(strcmp(message, "exit") == 0 || getchar()==EOF) {
